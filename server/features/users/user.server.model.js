decc502291d7b8e2d0825d380ae60d293e30ccd4
Blake Hagen <blakehagen@gmail.com>
2015-12-28 13:19:26 -0700

    updated user model

--- server/features/users/user.server.model.js
+++ server/features/users/user.server.model.js
@@ -5,15 +5,25 @@ var UserSchema = new Schema({
     name: { type: String, required: true, unique: true },
     googleId: { type: String, required: true, unique: true },
     image: { type: String },
-    currentLocation: {
-        lattitude: { type: String },
-        longitude: { type: String }
-    },
-    lastKnownLocation: {
-        lattitude: { type: String },
-        longitude: { type: String }
-    },
+    currentLocation: {type: [Number]}, // --> **** NOTE **** MONGOOSE REQUIRES [LONG, LAT] FORMAT // GOOGLE MAPS REQUIRES OPPOSITE [LAT, LONG] //
+    lastKnownLocation: {type: [Number]},
+    htmlverified: { type: String },
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date, default: Date.now },
     connections: [{ type: Schema.Types.ObjectId, ref: 'User' }],
 });
+
+// SET created_at PARAMETER EQUAL TO CURRENT TIME //
+UserSchema.pre('save', function (next) {
+    now = new Date();
+    this.updated_at = now;
+    if (!this.created_at) {
+        this.created_at = now
+    }
+    next();
+});
+
+// INDEX THIS SCHEMA IN 2DSPHERE FORMAT (FOR PROXIMITY SEARCHES) //
+UserSchema.index({ location: '2dsphere' });
 
 module.exports = mongoose.model('User', UserSchema);
