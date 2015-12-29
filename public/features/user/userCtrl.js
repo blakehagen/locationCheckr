b8f3bc9492954e27e9e5f49fe31604ae01f67d95
Blake Hagen <blakehagen@gmail.com>
2015-12-29 14:11:30 -0700

    mapping connections current location

--- public/features/user/userCtrl.js
+++ public/features/user/userCtrl.js
@@ -1,18 +1,28 @@
-angular.module('locationTracker').controller('userCtrl', function ($scope, $stateParams, geolocation, userService, mapService) {
-
+angular.module('locationTracker').controller('userCtrl', function ($scope, $stateParams, geolocation, userService) {
+    
+    // SET USER ID TO SCOPE//
     $scope.user = $stateParams.id;
 
+    // GET USER DATA/LOCATION ON SIGN IN //
     $scope.getUserData = function () {
+        $scope.loading = true;
         userService.getUser($scope.user).then(function (user) {
-            console.log(user);
+            // console.log(user);
+            $scope.myConnections = user.connections;
+            // console.log('myConnections ', $scope.myConnections);
+
+            $scope.getMyLocation();
         })
     };
-    // GET USER DATA ON LOGIN //
+
+    // RUN GET USER FUNCTION //
     $scope.getUserData();
     
-    // GET USER'S LOCATION //
+    // GET USER'S LOCATION/MAP VIA GOOGLE MAPS //
+    // THIS FUNCTION RUNS AS PART OF getUserData FUNCTION //
     $scope.getMyLocation = function () {
         $scope.coords = geolocation.getLocation().then(function (data) {
+            $scope.loading = false;
             // console.log({ lat: data.coords.latitude, long: data.coords.longitude });
 
             var map = new google.maps.Map(document.getElementById('map'), {
@@ -25,28 +35,53 @@ angular.module('locationTracker').controller('userCtrl', function ($scope, $stat
                 lng: data.coords.longitude
             };
 
-            var marker = new google.maps.Marker({
+            var myLocation = new google.maps.Marker({
                 position: pos,
                 map: map,
-                title: "Big Map",
+                title: "My Location",
                 icon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
             });
 
-            marker.setPosition(pos);
-            marker.setContent('You are here!');
+            myLocation.setPosition(pos);
+            
+            // CONVERT CONNECTION LOCATIONS TO GOOGLE MAPS FORMAT //
+            $scope.connectionsCurrentLocations = [];
+            for (var i = 0; i < $scope.myConnections.length; i++) {
+                var connection = $scope.myConnections[i];
+                $scope.connectionsCurrentLocations.push({
+                    latlon: new google.maps.LatLng(connection.currentLocation[1], connection.currentLocation[0]),
+                    name: connection.name
+                })
+            }
+            // console.log($scope.connectionsCurrentLocations);
 
+            $scope.connectionsCurrentLocations.forEach(function (n, i) {
+                var marker = new google.maps.Marker({
+                    position: n.latlon,
+                    map: map,
+                    title: n.name,
+                    icon: "http://maps.google.com/mapfiles/ms/icons/green-dot.png",
+                });
+            });
+            
             // infoWindow.setPosition(pos);
             // infoWindow.setContent('You are here!');
-
+            
+            // SET CURRENT LOCATION TO SEND TO DB // --> NOTICE FORMAT IS OPPOSITE OF GOOGLE MAPS FORMAT
             $scope.myCurrentLocation = {
                 currentLocation: [data.coords.longitude, data.coords.latitude]
             };
+            // SEND CURRENT LOCATION INFO TO DB //
             userService.updateMyLocation($scope.user, $scope.myCurrentLocation).then(function (response) {
-                console.log(response);
+                // console.log(response);
             })
         })
     };
     
+
+
+
+    // FROM GOOGLE MAPS DOCS --> USED AS REFERENCE //
 
     // function initMap() {
     //     var map = new google.maps.Map(document.getElementById('map'), {
