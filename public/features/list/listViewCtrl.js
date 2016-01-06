5e35dbe7c954efd1f4485fb1b91517291b59d421
Blake Hagen <blakehagen@gmail.com>
2016-01-05 21:57:39 -0700

    background on list view and styles and favicon

--- public/features/list/listViewCtrl.js
+++ public/features/list/listViewCtrl.js
@@ -4,9 +4,12 @@ angular.module('locationTracker').controller('listViewCtrl', function ($rootScop
         userService.getConnectionLocations($rootScope.user).then(function (response) {
             console.log(response.connections);
             $scope.listViewData = [];
+            $scope.offlineData = [];
             for (var i = 0; i < response.connections.length; i++) {
                 if (response.connections[i].status === 'active') {
                     $scope.listViewData.push(response.connections[i]);
+                } else {
+                    $scope.offlineData.push(response.connections[i]);
                 }
             }
             // console.log($scope.listViewData);
